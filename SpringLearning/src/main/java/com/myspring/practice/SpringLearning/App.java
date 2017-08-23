d18e3fed9b45b7288dd29fceb18ffa945a024968
Rajath Kumar <rkumar5@walmartlabs.com>
2017-08-23 15:40:03 +0530

    First Commit

---
+++ SpringLearning/src/main/java/com/myspring/practice/SpringLearning/App.java
@@ -0,0 +1,13 @@
+package com.myspring.practice.SpringLearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
